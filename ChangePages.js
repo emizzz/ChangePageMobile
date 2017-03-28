72868563583c5746ee5efecef7b779ef56dc9f22
emizzz <emilianoversini@hotmail.it>
2017-03-28 11:45:02 +0200

    Add files via upload

--- ChangePages.js
+++ ChangePages.js
@@ -4,12 +4,19 @@ $(document).ready(function(){
     
     //if url change
     window.addEventListener("hashchange", function(e){
-        var oldUrl = e.oldURL.split("#")[1] || $('#page1')[0].id;
-        var newUrl = e.newURL.split("#")[1];
-        to_hide_div = $("body").find("#" + oldUrl);
-        to_show_div = $("body").find("#" + newUrl);
+        try{
+            var newUrl = e.newURL.split("#")[1];
+            newUrl != '' ? newUrl : (function(){throw "empty anchor"}());
+            
+            to_hide_divs = $(".page").css("display", "none");
+            to_show_div = $("body").find("#" + newUrl);
+
+            $(to_hide_divs).css("display", "none");
+            $(to_show_div).css("display", "block");
+        }
+        catch(err){
+            console.warn(err);
+        }
         
-        $(to_hide_div).css("display", "none");
-        $(to_show_div).css("display", "block");
     });
 });
